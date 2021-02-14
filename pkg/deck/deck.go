030a891a11e847e1168c4355e50960be5d8555b4
mova <valeriy.molchanov.77@gmail.com>
2021-02-14 21:52:31 +0200

    package deck

--- pkg/deck/deck.go
+++ pkg/deck/deck.go
@@ -1,1 +1,18 @@
 package deck
+
+// Card represents a playing card
+type Card struct {
+	Suit  Suit
+	Value int
+}
+
+// Suit is the playing card suit
+type Suit int
+
+// All suits of the playing card
+const (
+	Clubs Suit = iota
+	Diamonds
+	Hearts
+	Spades
+)
